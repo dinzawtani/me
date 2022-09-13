34b5b5a64c3965b9adf7bc858cc0399e96f98c4d
Dinesh Awtani <dineshawtaniblogs@gmail.com>
2022-09-13 15:44:28 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -24,7 +24,7 @@ window.onload = function() {
   var messages = [
     'Hey there 👋',
     'I\'m Dinesh Awtani',
-    'I'm a illustrator',
+    'I\'m a illustrator',
     'I\'m currently accepting freelance work.<br> You can contact me at <a href="mailto:dinesh@awtani.co">Dinesh@awtani.co</a>',
     '<a target="_blank" href="https://twitter.com/dinzawtani">twitter.com/dinzawtani</a><br><a target="_blank" href="https://behance.net/awtani">behance.net/awtani</a><br><a target="_blank" href="https://dribbble.com/awtani">dribbble.com/awtani</a>',
     getCurrentTime(),
